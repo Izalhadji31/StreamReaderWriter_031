0397c7c161427bb008e848415593606874855427
Izalhadji31 <ryzalhadji@gmail.com>
2023-07-13 16:56:50 +0700

    membuat try

--- manualExeption/manualExeption.cpp
+++ manualExeption/manualExeption.cpp
@@ -2,3 +2,8 @@
 using namespace std;
 
 int main() {
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5; // lempar value untuk di eksekusi oleh catch
+        cout << "Pernyataan todak di eksekusi" << endl;
+    }
