0046215d9a7227791e7646fd4cffefddd67cd8f9
Izalhadji31 <ryzalhadji@gmail.com>
2023-07-13 16:37:26 +0700

    membuat input untuk memasukan ke dalam file yang telah di buka

--- StreamReaderWriter_031.cpp
+++ StreamReaderWriter_031.cpp
@@ -9,4 +9,13 @@ int main() {
 	ofstream outfile;
 	//open file
 	outfile.open("ContohFile.txt");
+
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	while (true) {
+		cout << "- ";
+		getline(cin, baris);
+
+	}
 }
